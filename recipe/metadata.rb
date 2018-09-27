b086a8a5852eb4bedc0319d97d73e3f69d88fe98
anjukrish555 <39890824+anjukrish555@users.noreply.github.com>
2018-09-27 16:33:26 -0400

    Add files via upload

---
+++ recipe/metadata.rb
@@ -0,0 +1,8 @@
+name 'petclinic'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures learn_chef_apache2'
+long_description 'Installs/Configures learn_chef_apache2'
+version '0.1.0'
+
